45c790e67493de07d7fffe27d9afc91a09f716af
haroonym <71009111+haroonym@users.noreply.github.com>
2022-01-25 20:00:32 +0100

    Logo und Manifest erstellt

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p class="text-h3 text-center mt-4">
+      <v-icon class="text-h3 red--text">mdi-heart</v-icon> IManager
+      <v-icon class="text-h3 red--text">mdi-heart</v-icon>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    manifestOptions: {
+      name: 'IManager',
+      short_name: 'PWA Project',
+      theme_color: '#F1333F',
+      background_color: '#353434',
+      icons: [
+        { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+        { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      ],
+    },
+  },
+};
